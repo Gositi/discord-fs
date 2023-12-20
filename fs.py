bb6ea38af5c4087e0016c9a348e32db38a3b0b41
gositi <simon.bryntse@gmail.com>
2023-12-20 15:04:00 +0100

    _Kinda_ works...

---
+++ fs.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+#Taken from https://github.com/skorokithakis/python-fuse-sample/blob/master/passthrough.py
+
+import os
+import sys
+import errno
+
+from fuse import FUSE, FuseOSError, Operations, fuse_get_context
+
+class Passthrough(Operations):
+    def __init__(self, root):
+        self.root = root
+
+    # Helpers
+    # =======
+
+    def _full_path(self, partial):
+        if partial.startswith("/"):
+            partial = partial[1:]
+        path = os.path.join(self.root, partial)
+        return path
+
+    # Filesystem methods
+    # ==================
+    def getattr(self, path, fh=None):
+        full_path = self._full_path(path)
+        st = os.lstat(full_path)
+        return dict((key, getattr (st, key)) for key in ('st_atime', 'st_ctime',
+                     'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
+
+    def readdir(self, path, fh):
+        full_path = self._full_path(path)
+
+        dirents = ['.', '..']
+        if os.path.isdir(full_path):
+            dirents.extend(os.listdir(full_path))
+        for r in dirents:
+            yield r
+
+    def statfs(self, path):
+        full_path = self._full_path(path)
+        stv = os.statvfs(full_path)
+        return dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
+            'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
+            'f_frsize', 'f_namemax'))
+
+    # File methods
+    # ============
+
+    def open(self, path, flags):
+        full_path = self._full_path(path)
+        return os.open(full_path, flags)
+
+    def create(self, path, mode, fi=None):
+        uid, gid, pid = fuse_get_context()
+        full_path = self._full_path(path)
+        fd = os.open(full_path, os.O_WRONLY | os.O_CREAT, mode)
+        os.chown(full_path,uid,gid) #chown to context uid & gid
+        return fd
+
+    def read(self, path, length, offset, fh):
+        os.lseek(fh, offset, os.SEEK_SET)
+        return os.read(fh, length)
+
+    def write(self, path, buf, offset, fh):
+        os.lseek(fh, offset, os.SEEK_SET)
+        return os.write(fh, buf)
+
+    def truncate(self, path, length, fh=None):
+        full_path = self._full_path(path)
+        with open(full_path, 'r+') as f:
+            f.truncate(length)
+
+    def flush(self, path, fh):
+        return os.fsync(fh)
+
+    def release(self, path, fh):
+        return os.close(fh)
+
+    def fsync(self, path, fdatasync, fh):
+        return self.flush(path, fh)
+
+
+def main(mountpoint, root):
+    FUSE(Passthrough(root), mountpoint, nothreads=True, foreground=True, allow_other=False)
+
+
+if __name__ == '__main__':
+    main(sys.argv[2], sys.argv[1])
